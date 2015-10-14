ad9d83c714eff73039dec5468dff00f49ff18917
furkanozbay <ozbayfurkan@outlook.com>
2015-10-14 03:17:45 +0300

    Advanced Homework 3 Added

---
+++ Advanced Programming/Homework3/Homework3/Matrix.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    class Matrix
+    {
+        public int[,] numbers;
+
+        public Matrix()
+        {
+          
+        }
+        public Matrix(int row, int column) {
+            this.numbers = new int[row, column];
+        }
+        public int getSize()
+        {
+            return this.numbers.GetLength(0);
+        }
+        public void writeNumbers()
+        {
+            for (int i = 0; i < this.getSize(); i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < this.getSize(); j++)
+                {
+                    Console.Write(this.numbers[i, j] + " ");
+                }
+            }
+        }
+        public static Matrix createSubMatrix(Matrix matrix, int row, int column)
+        {
+            Matrix mat = new Matrix(matrix.getSize() - 1, matrix.getSize() - 1);
+            int r = -1;
+            for (int i = 0; i < matrix.getSize(); i++)
+            {
+                if (i == row)
+                    continue;
+                r++;
+                int c = -1;
+                for (int j = 0; j < matrix.getSize(); j++)
+                {
+                    if (j == column)
+                        continue;
+                    mat.numbers[r, ++c] = matrix.numbers[i, j];
+                }
+            }
+            return mat;
+            
+
+        }
+    }
+}
---
+++ Advanced Programming/Homework3/Homework3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            while (true)
+            {
+                Console.Write("Enter the dimension of matrix(Bigger than 1, Lower than 10): ");
+                try
+                {
+                    int dimension = int.Parse(Console.ReadLine());
+
+                    if (dimension < 1 || dimension > 10)
+                    {
+                        Console.WriteLine();
+
+                        continue;
+                    }
+                    else
+                    {
+                        int[,] numbers = new int[dimension, dimension];
+
+                        for (int i = 0; i < dimension; i++)
+                        {
+                            for (int j = 0; j < dimension; j++)
+                            {
+                                numbers[i, j] = random.Next(1, 9);
+                            }
+                        }
+                        Matrix matrix = new Matrix();
+                        matrix.numbers = numbers;
+                        Console.WriteLine(determinant(matrix));
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Dimension must be bigger than 1, lower than 10.\n");
+                }
+            }
+        }
+        public static int determinant(Matrix matrix)
+        {
+            int sum = 0;
+
+            //To display the initial matrix
+            matrix.writeNumbers();
+            Console.WriteLine();
+            //To display the initial matrix
+
+            if (matrix.getSize() < 1)
+            {
+                return 0;
+            }
+            else if (matrix.getSize() == 1)
+            {
+                return matrix.getSize();
+            }
+            else if (matrix.getSize() == 2)
+            {
+                return matrix.numbers[0, 0] * matrix.numbers[1, 1] - matrix.numbers[0, 1] * matrix.numbers[1, 0];
+            }
+            int sign;
+            for (int i = 0; i < matrix.getSize(); i++)
+            {
+                sign = (i % 2 == 0) ? 1 : -1;
+                sum += sign * matrix.numbers[0, i] * determinant(Matrix.createSubMatrix(matrix, 0, i));
+            }
+
+
+            return sum;
+        }
+
+    }
+}
